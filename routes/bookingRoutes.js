83bc0042850674c5ece567b081e289d200976434
Makhubele Langavi <mlangaviclyde@gmail.com>
2026-03-31 22:04:54 +0200

    Configure multer for memory storage in booking routes

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -3,8 +3,9 @@ import multer from "multer";
 import { handleBooking } from "../controllers/bookingController.js";
 
 const router = express.Router();
-const upload = multer(); // Initialize multer to handle memory storage
+const upload = multer({ storage: multer.memoryStorage() });
 
+// POST /api/book
 router.post("/", upload.array("images", 3), handleBooking);
 
 export default router;
